7e431b1c2c401084f045824c324582d4930ce2f0
Rodrigo <Rodrigo@PC-RODRIGO>
2024-09-23 23:46:41 -0300

    Agregar archivos de proyecto.

---
+++ VerificadorEdad0/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VerificadorEdad0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = "Ingrese su edad en el siguiente recuadro:";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int edad1;
+            VerificadorEdad verificador = new VerificadorEdad();
+
+            while (true)
+            {
+                if (int.TryParse(textBox1.Text, out edad1))
+                {
+                    label2.Text = verificador.Verificar(edad1);
+                    break;
+                }
+                else
+                {
+                    label2.Text = "Ingrese un número válido";
+                    break;
+                }
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
---
+++ VerificadorEdad0/edad.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VerificadorEdad0
+{
+    public class VerificadorEdad
+    {
+        public string Verificar(int edad)
+        {
+            if (edad >= 18)
+            {
+                return "Acceso concedido";
+            }
+            else
+            {
+                return "Acceso no concedido";
+            }
+        }
+    }
+}
